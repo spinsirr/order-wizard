6b1049e7694d191a5154295319ed233138b11a4b
spinsirr <spencerzhyp@gmail.com>
2026-01-25 00:24:37 -0800

    feat: add Options page for FB template settings

---
+++ apps/extension/src/options/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Options } from './Options';
+import '../index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Options />
+  </React.StrictMode>
+);
